c656f4bdb4e7840548c3d7f1ae1d202199f0947b
Francisco Delgado <frankjdelgado@gmail.com>
2015-06-01 21:36:03 -0430

    spanish translations for Laravel 5.0

---
+++ es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Las contraseñas deben tener al menos seis carácteres y coincidir con la confirmación.",
+	"user" => "No hemos podido encontrar un usuario con ese correo electrónico.",
+	"token" => "Este token de restablecimiento de contraseña no es válido.",
+	"sent" => "¡Te hemos enviado tu enlace de restablecimiento de contraseña!",
+	"reset" => "¡Tu contraseña ha sido restablecida!",
+
+];
